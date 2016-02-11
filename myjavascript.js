44510c574c4fde51e2134f29bccb9c9e7929f4c9
Lauren Klose <lauren.klose@journalism.cuny.edu>
2016-02-11 13:53:08 -0500

    Initial commit

--- myjavascript.js
+++ myjavascript.js
@@ -1,18 +1,1 @@
-var first = 14;
-var second = 28;
-
-var third = first + second;
-
-var fourth = first * second;
-
-var fifth = second / first;
-
-var sixth = second - first;
-
-document.write('<p> When I add ' +first+ ' and ' +second+ ' I get ' +third+ '.</p>');
-
-document.write('<p> When I multiply ' +first+ ' and ' +second+ ' I get ' +fourth+ '.</p>');
-
-document.write('<p> When I divide ' +second+ ' by ' +first+ ' I get ' +fifth+ '.</p>');
-
-document.write('<p> When I subtract ' +first+ ' from ' +second+ ' I get ' +sixth+ '.</p>');
+Hello World
